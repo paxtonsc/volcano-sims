222459c6c03dae03ecbe90f2c811924c76190e5e
Paxton Scott <paxtonsc@stanford.edu>
2025-05-19 14:44:36 -0700

    updating helper functions and such.

--- notebooks/helper_code/lighthill.py
+++ notebooks/helper_code/lighthill.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+from scipy import signal
+
 
 def compute_ref_mapping(x_tri):
 	''' Compute jacobian
@@ -22,3 +25,104 @@ def compute_ref_mapping(x_tri):
 	_M[...,1:2,1:2] = _a00
 	_M /= (_a00 * _a11 - _a01 * _a10)
 	return _M
+
+
+
+def calculate_pressure_as_volume_integral(X, Y, Z, file_index_list, x_obs, points, source, t_range, max_r, c0):
+    dv = (X[1] - X[0]) * (Y[1] - Y[0]) * (Z[1] - Z[0])
+
+    print(f"DV size is {dv}")
+
+    rs = []
+    src_values = []
+    p_t = np.zeros(len(file_index_list))
+
+    for t_idx in file_index_list:
+        for i in range(len(X)):
+            for  j in range(len(Y)):
+                for k in range(len(Z)):
+                    # source position
+                    y_src = np.array([X[i], Y[j], Z[k]])
+
+                    # distance from source to observation point
+                    r = np.linalg.norm(x_obs - y_src)
+                    t_ret = t_range[t_idx] - r / c0
+
+                    # a little hacky, but ignore any contributions with r the max value of Y
+                    if np.linalg.norm([X[i], Z[k]]) > max_r:
+                        continue
+
+                    # For the moment let's skip summing values inside the conduit. 
+                    if Y[k] < 0:
+                        continue
+
+                    if t_ret >= 0 and t_ret < t_range[-1]:
+                        interpolator = RegularGridInterpolator(points, source, method='linear', bounds_error=False, fill_value=0)
+
+                        src_val = interpolator([t_ret, Y[j], np.linalg.norm([X[i], Z[k]])])
+
+                        if not np.isnan(src_val):
+                            p_t[t_idx] += (src_val / (4 * np.pi * r) ) * dv
+                            rs.append(r)
+                            src_values.append(src_val)
+
+        if t_idx % 10 == 0:
+            print(f"Finished time index {t_idx} of {len(file_index_list)}")
+
+    print(f"Number of contributions: {len(rs)}") 
+    print(f"r average: {np.average(rs)}")
+    print(f"Max source value: {max(np.asarray(src_values))}")
+    return p_t
+
+
+def calculate_surface_integral(div_T, a, t_idx, t_range, x_obs, c0, points):
+    N_theta, N_phi = 50, 50
+
+    theta = np.linspace(0, np.pi/3, N_theta)
+    phi = np.linspace(0, 2*np.pi, N_phi)
+    a = 100 # [m]
+
+    dphi = phi[1] - phi[0]
+    dtheta = theta[1] - theta[0]
+
+    p_t = 0
+
+    for i in range(N_theta):
+        for j in range(N_phi):
+            ds = a**2 * np.sin(phi[j]) * dphi * dtheta
+            x = a * np.sin(phi[j]) * np.cos(theta[i])
+            y = a * np.sin(phi[j]) * np.sin(theta[i])
+            z = a * np.cos(phi[j])
+
+            r = np.linalg.norm(np.array([x, y, x]) - x_obs)
+
+            t_ret = t_range[t_idx] - r / c0
+
+            if t_ret > 0 and t_ret < t_range[-1]:
+
+                interpolator = RegularGridInterpolator(points, div_T, method='linear', bounds_error=False, fill_value=0)
+
+                src_val_0 = interpolator([0, t_ret, z, np.linalg.norm([x, y])])
+                src_val_1 = interpolator([1, t_ret, z, np.linalg.norm([x, y])])
+
+                if not np.isnan(src_val_0) and not np.isnan(src_val_1):
+                    p_t += 1/a * (src_val_0*z + src_val_1*np.linalg.norm([x, y])) / (4 * np.pi * r) * ds
+    
+    return p_t
+
+def highpass(p, lowcut=1.0):
+    fs = 10  # Sampling frequency (Hz)
+
+    # Step 2: Define band-pass filter parameters
+    lowcut = 1.0  # Lower cutoff frequency (Hz)
+
+    order = 5  # Filter order
+
+    # Normalize cutoff frequencies to Nyquist frequency (fs/2)
+    nyquist = 0.5 * fs
+    low = lowcut / nyquist
+
+    # Step 3: Design Butterworth band-pass filter
+    b, a = signal.butter(order, low, btype='high')
+
+    return signal.filtfilt(b, a, p)
